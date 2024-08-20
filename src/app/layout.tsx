ab6a6a1d02b440c86213fe4b59c7336b7b60e20c
Farid Vatani <faridvatani@gmail.com>
2024-08-20 20:38:30 +0330

    feat: Implement header section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Calistoga } from "next/font/google";
 import "@/styles/globals.css";
+import { twMerge } from "tailwind-merge";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "Portly",
@@ -16,7 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning>
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans",
+        )}
+        suppressHydrationWarning
+      >
         {children}
       </body>
     </html>
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+
+const navItems = [
+  { href: "#", title: "Home" },
+  { href: "#", title: "Projects" },
+  { href: "#", title: "About" },
+  { href: "#", title: "Contact" },
+];
+
 export const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <header className="fixed flex justify-center items-center top-3 w-full">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        {navItems.map((item, index) => (
+          <Link key={index} href={item.href} className="nav-item active">
+            {item.title}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
 };
