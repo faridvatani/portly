a2d32b9517406d04df39c323d4cef22eb5c132be
Farid Vatani <faridvatani@gmail.com>
2024-08-27 18:03:23 +0330

    feat: Implement animation to Hero section

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,32 +1,55 @@
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface HeroOrbitProps {
   size: number;
   rotation: number;
+  shouldOrbit?: boolean;
+  orbitDuration?: number;
+  shouldSpin?: boolean;
+  spinDuration?: number;
 }
 
 const HeroOrbit = ({
   children,
   size,
   rotation,
+  shouldOrbit = false,
+  orbitDuration = 10,
+  shouldSpin = false,
+  spinDuration,
 }: PropsWithChildren<HeroOrbitProps>) => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <div
-        className="flex items-start justify-start"
+        className={twMerge(shouldOrbit && "animate-spin")}
         style={{
-          height: `${size}px`,
-          width: `${size}px`,
-          transform: `rotate(${rotation}deg)`,
+          animationDuration: `${orbitDuration}s`,
         }}
       >
         <div
-          className="inline-flex"
+          className="flex items-start justify-start"
           style={{
-            transform: `rotate(${rotation * -1}deg)`,
+            height: `${size}px`,
+            width: `${size}px`,
+            transform: `rotate(${rotation}deg)`,
           }}
         >
-          {children}
+          <div
+            className={twMerge(shouldSpin && "animate-spin")}
+            style={{
+              animationDuration: `${spinDuration}s`,
+            }}
+          >
+            <div
+              className="inline-flex"
+              style={{
+                transform: `rotate(${rotation * -1}deg)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
